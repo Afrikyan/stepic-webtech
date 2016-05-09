c8605bce0c966fb6898beac68a33cc0029066524
Rinnve <rinnve@yandex.ru>
2016-05-09 17:23:05 +0300

    Fixed handling of empty query parameters

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-import urlparse
+import urllib.parse as urlparse
 
 def application(environ, start_response):
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    parameters = urlparse.parse_qsl(environ.get("QUERY_STRING"))
+    parameters = urlparse.parse_qsl(environ.get("QUERY_STRING"), keep_blank_values=True)
     body = ["{}={}".format(par, val) for (par, val) in parameters]
     body = "\r\n".join(body)
     start_response(status, headers)
