97fcf64732340b799b41965b76d7a35ce0484240
Rinnve <rinnve@yandex.ru>
2016-05-09 17:14:39 +0300

    Minor changes to gunicorn config file for hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,8 +1,8 @@
 # GUnicorn config for hello.py
 CONFIG = {                                                                      
-    # 'mode': 'wsgi',                                                           
+    'mode': 'wsgi',                                                           
     'working_dir': '/home/box/web',                                           
-    # 'python': '/usr/bin/python',                                              
+    'python': '/usr/bin/python',                                              
     'args': (                                                                   
         '--bind=0.0.0.0:8080',                                              
         '--workers=16',                                                         
